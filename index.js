1f93d43b071a54baaf241b889f70bdea098fbab8
Madeliene <madelienestiller@gmail.com>
2023-10-25 12:51:13 +1000

    array filter lab complete

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution here
+
+
+// function findMatching(drivers) {
+//     for (const driverName of drivers) {
+//         if (drivers.name === driverName) {
+//             return (drivers.driverName);
+//         }
+//         return [""];
+//     }
+// }
+
+// findMatching(drivers, "Bobby", "Sammy")
+
+
+
+function findMatching(drivers, string) {
+    return drivers.filter(function (driver){
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+console.log(drivers)
+
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function (driver){
+        return driver.slice(0, 2) === string;
+    });
+}
+
+console.log(drivers)
+
+
+
+
+function matchName(drivers, string) {
+    return drivers.filter(function (driver) {
+        return driver.name === string;
+    });
+}
+
+console.log(drivers)
